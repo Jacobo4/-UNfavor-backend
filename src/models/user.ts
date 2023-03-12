6dc2ec209ee87a287275aea7b9c6cec688bb9a52
Leandro Gomez <flopez@unal.edu.co>
2023-03-12 01:00:45 -0500

    user db schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,15 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-var UserSchema = Schema({
-	name: String,
-    age: Number,
-    phone: String,
+const UserSchema = Schema({
+	username: String,
+    name: String,
     email: String,
+    phone: String,
+    age: Number,
+    user_reviews_num: Number,
+    user_reviews_sum: Number,
+    user_favors:[Schema.Types.ObjectId]
 }, {collection: "users"});
 
 module.exports = mongoose.model("DataBase", UserSchema);//Nombre de la coleccion de la DB
