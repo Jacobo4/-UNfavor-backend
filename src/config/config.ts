f73a00c379cfc24c83d477cce2791db108fce0b0
NickGalindo <ngalindoc@unal.edu.co>
2023-05-21 21:17:43 -0500

    fixed exposed url

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'root';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@unfavor.3fcgsdk.mongodb.net/DataBase`;
+const MONGO_REMOTE_URL = process.env.MONGO_REMOTE_URL;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_REMOTE_URL}`;
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000;
 const VECTOR_DB_URL = process.env.VECTOR_DB_URL;
 
