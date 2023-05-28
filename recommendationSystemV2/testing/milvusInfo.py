b60a50816e0fc3085ffc0acff9f8b34ead5b7721
NickGalindo <ngalindoc@unal.edu.co>
2023-05-27 21:53:52 -0500

    Steroided up recommendation system

--- recommendationSystemV2/testing/milvusInfo.py
+++ recommendationSystemV2/testing/milvusInfo.py
@@ -4,7 +4,7 @@ from pymilvus import utility
 setup.connectMilvus()
 col = setup.setupMilvusCollection()
 
-print(f"collections: {utility.list_collections}")
+print(f"collections: {utility.list_collections()}")
 print(f"entities: {col.num_entities}")
 
 col.release()
