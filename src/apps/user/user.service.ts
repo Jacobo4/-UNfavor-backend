3ba11d9e1a4df5613dc9217655ef53776bbe03ce
Ivan Yared Lombana Lozano <ilombana@unal.edu.co>
2023-05-29 16:07:35 -0500

    Update user fix

--- src/apps/user/user.service.ts
+++ src/apps/user/user.service.ts
@@ -114,7 +114,14 @@ const userService = {
     // Buscar y actualizar el usuario en la base de datos, newUserData es un objeto con los elementos a actualizar.
     const updateUser: IUser = await User.findByIdAndUpdate(
       userId,
-      { $set: filteredUserData }, // Utilizar el operador $set para actualizar sólo los campos permitidos
+      {
+        name: filteredUserData.name,
+        phone: filteredUserData.phone,
+        "favor.title": filteredUserData.favor.title,
+        "favor.description": filteredUserData.favor.description,
+        "favor.location": filteredUserData.favor.location,
+        "favor.category": filteredUserData.favor.category
+      }, // Utilizar el operador $set para actualizar sólo los campos permitidos
       { new: true } // Para obtener el objeto actualizado en la respuesta
     ).select('-password -admin').exec();
 
